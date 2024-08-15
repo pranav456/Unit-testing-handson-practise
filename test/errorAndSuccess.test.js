22e06ade7d9a876e0fbf12bd8b9d13343adaa3bb
Pranav <bhargavpranav3@gmail.com>
2024-08-15 23:41:31 +0530

    errorAndSuccess and loan-emi testcases added

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -5,8 +5,16 @@ import '../src/SuccessAndError/Error.js';
 
 describe('Success screen ', () => {
   // Write test cases inside this block
+  it('success message exists', async ()=>{
+    const el = await fixture(html`<loan-success></loan-success>`);
+    expect(el).dom.exist;
+  })
 });
 
 describe('error screen', () => {
   // Write test cases inside this block
+  it('error message exists', async () => {
+    const el = await fixture(html`<loan-error></loan-error>`);
+    expect(el).dom.exist;
+  })
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -3,4 +3,23 @@ import '../src/LoanEMIDetails/LoanEMIDetails.js';
 
 describe('Loan EMI details', () => {
   // Write test cases inside this block
-});
+  it('renders the component', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    expect(el).to.exist; // Check if the element is rendered
+  });
+
+  it('displays the correct EMI details', async () => {
+    // Mock the data in localStorage
+    const mockEMIData = {
+      interestRate: 5.5,
+      monthlyEMI: 2000,
+      principal: 100000,
+      interest: 5000,
+      totalAmount: 105000,
+    };
+    localStorage.setItem('emi', JSON.stringify(mockEMIData));
+    const emiData = JSON.parse(localStorage.getItem('emi'))
+    const el = await fixture(html`<loanemi-details ._data=${emiData}></loanemi-details`)
+    expect(el._data).to.deep.equal(emiData)      
+  });
+})
